2e0107ac3a17f09a9a59a86a0a442319cc26cc7d
Leonardo Bastos <leonardobastos04@gmail.com>
2023-10-26 02:26:27 -0300

    feat: form component, modal content, create store

--- Backend/core/settings.py
+++ Backend/core/settings.py
@@ -77,7 +77,7 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
     "http://127.0.0.1:5173",
     "http://127.0.0.1",
-    "http://127.0.0.1:8000"
+    "http://127.0.0.1:8000",
 ]
 
 CSRF_TRUSTED_ORIGINS = [
